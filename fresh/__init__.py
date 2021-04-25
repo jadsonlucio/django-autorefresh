4d3544458ae244438a6a014dc9053fec2edb06eb
jadsonlucio <jadson.santos@arapiraca.ufal.br>
2021-04-25 19:07:48 -0300

    upgrade version

--- fresh/__init__.py
+++ fresh/__init__.py
@@ -4,4 +4,4 @@ from fresh.filter import filter_fresh_request
 server_logging = logging.getLogger("django.server")
 server_logging.addFilter(filter_fresh_request)
 
-__version__ = "1.0.6"  # noqa
+__version__ = "1.0.7"  # noqa
